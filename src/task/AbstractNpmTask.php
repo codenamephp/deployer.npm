0cb19ea40a53f0b65d288f6ca5ac1f0b75ce1d95
Bastian Schwarz <bastian@codename-php.de>
2022-01-20 21:22:19 +0100

    Extracted arguments to function so they can be set easily in concrete tasks. Also disabled funding message
    Signed-off-by: Bastian Schwarz <bastian@codename-php.de>

--- src/task/AbstractNpmTask.php
+++ src/task/AbstractNpmTask.php
@@ -1,4 +1,19 @@
 <?php declare(strict_types=1);
+/*
+ *   Copyright 2022 Bastian Schwarz <bastian@codename-php.de>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
 namespace de\codenamephp\deployer\npm\task;
 
@@ -21,7 +36,7 @@ abstract class AbstractNpmTask implements iTask {
     public iRunner $runner = new WithDeployerFunctions(),
     iGet           $deployer = new All()
   ) {
-    $this->command = new Command((string) $deployer->get('npm:binary', 'npm'), [$this->getNpmCommand(), '--prefix {{release_path}}']);
+    $this->command = new Command((string) $deployer->get('npm:binary', 'npm'), [$this->getNpmCommand(), ...$this->getArguments()]);
   }
 
   /**
@@ -32,6 +47,17 @@ abstract class AbstractNpmTask implements iTask {
    * @return string
    */
   abstract public function getNpmCommand() : string;
+
+  /**
+   * Gets the arguments to pass to the command. Override this and add you commands and options as needed.
+   *
+   * The array has to be numerical so it can be expanded in the constructor
+   *
+   * @return array<int,string>
+   */
+  public function getArguments() : array {
+    return ['--prefix {{release_path}}', '--fund=false'];
+  }
 
   public function __invoke() : void {
     $this->runner->run($this->command);
--- test/task/AbstractNpmTaskTest.php
+++ test/task/AbstractNpmTaskTest.php
@@ -1,4 +1,19 @@
 <?php declare(strict_types=1);
+/*
+ *   Copyright 2022 Bastian Schwarz <bastian@codename-php.de>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
 namespace de\codenamephp\deployer\npm\test\task;
 
@@ -30,7 +45,7 @@ final class AbstractNpmTaskTest extends TestCase {
     $this->sut = $this->getMockForAbstractClass(AbstractNpmTask::class, [$runner, $deployer]);
 
     self::assertSame($runner, $this->sut->runner);
-    self::assertEquals(new Command('npm', ['', '--prefix {{release_path}}']), $this->sut->command);
+    self::assertEquals(new Command('npm', ['', '--prefix {{release_path}}', '--fund=false']), $this->sut->command);
   }
 
   public function test__construct_withNonStringFromGet() : void {
@@ -41,7 +56,7 @@ final class AbstractNpmTaskTest extends TestCase {
     $this->sut = $this->getMockForAbstractClass(AbstractNpmTask::class, [$runner, $deployer]);
 
     self::assertSame($runner, $this->sut->runner);
-    self::assertEquals(new Command('', ['', '--prefix {{release_path}}']), $this->sut->command);
+    self::assertEquals(new Command('', ['', '--prefix {{release_path}}', '--fund=false']), $this->sut->command);
   }
 
   public function test__invoke() : void {
@@ -49,5 +64,9 @@ final class AbstractNpmTaskTest extends TestCase {
     $this->sut->runner->expects(self::once())->method('run')->with($this->sut->command);
 
     $this->sut->__invoke();
+  }
+
+  public function testGetArguments() : void {
+    self::assertEquals(['--prefix {{release_path}}', '--fund=false'], $this->sut->getArguments());
   }
 }
